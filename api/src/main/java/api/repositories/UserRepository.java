538a4c6f2286c59a883299b81a11e45324cdf905
João <jvitor2000@outlook.com>
2022-04-14 19:26:37 -0300

    feature(#1:backend): Adiciona anotação de repository.

--- api/src/main/java/api/repositories/UserRepository.java
+++ api/src/main/java/api/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package api.repositories;
 
 import api.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }

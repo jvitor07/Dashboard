cd331ea0bcc07dee08bac16bd60b16650a35db81
João <jvitor2000@outlook.com>
2022-04-14 23:45:04 -0300

    test(#1:backend): Escreve testes para o endpoint de cadastrar usuário.

--- api/src/test/java/api/integration/controllers/UserControllerTest.java
+++ api/src/test/java/api/integration/controllers/UserControllerTest.java
@@ -18,10 +18,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application.properties")
@@ -60,5 +60,126 @@ public class UserControllerTest {
         ResponseDTO<User> responseBody = this.mapper.readValue(response.getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {});
         assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
         assertThat(expected).isEqualTo(responseBody);
+    }
+
+    @Order(2)
+    @Test
+    void itShouldReceiveErrorMessageWhenTryingToRegisterNewUserWithoutName() throws Exception {
+        User payload = this.userFactory.setName(null).newInstance();
+        ResponseDTO<User> expected = new ResponseDTO<>(List.of("É necessário informar um nome"), null);
+        MvcResult result = this.mockMvc.perform(post(USER_REGISTER_ROUTE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(payload)))
+                .andReturn();
+        MockHttpServletResponse response = result.getResponse();
+        ResponseDTO<User> responseBody = this.mapper.readValue(response.getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {});
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        assertThat(expected.getMessages()).containsAll(responseBody.getMessages());
+    }
+
+    @Order(3)
+    @Test
+    void itShouldReceiveErrorMessageWhenTryingToRegisterNewUserWithoutEmail() throws Exception {
+        User payload = this.userFactory.setEmail(null).newInstance();
+        ResponseDTO<User> expected = new ResponseDTO<>(List.of("É necessário informar um email"), null);
+        MvcResult result = this.mockMvc.perform(post(USER_REGISTER_ROUTE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(payload)))
+                .andReturn();
+        MockHttpServletResponse response = result.getResponse();
+        ResponseDTO<User> responseBody = this.mapper.readValue(response.getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {});
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        assertThat(expected.getMessages()).containsAll(responseBody.getMessages());
+    }
+
+    @Order(4)
+    @Test
+    void itShouldReceiveErrorMessageWhenTryingToRegisterNewUserWithoutPassword() throws Exception {
+        User payload = this.userFactory.setPassword(null).newInstance();
+        ResponseDTO<User> expected = new ResponseDTO<>(List.of("É necessário informar uma senha"), null);
+        MvcResult result = this.mockMvc.perform(post(USER_REGISTER_ROUTE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(payload)))
+                .andReturn();
+        MockHttpServletResponse response = result.getResponse();
+        ResponseDTO<User> responseBody = this.mapper.readValue(response.getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {});
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        assertThat(expected.getMessages()).containsAll(responseBody.getMessages());
+    }
+
+    @Order(5)
+    @Test
+    void itShouldReceiveErrorMessageWhenTryingToRegisterNewUserWithBlankName() throws Exception {
+        User payload = this.userFactory.setName("").newInstance();
+        ResponseDTO<User> expected = new ResponseDTO<>(List.of("É necessário informar um nome", "O nome deve conter entre 5 e 70 caracteres"), null);
+        MvcResult result = this.mockMvc.perform(post(USER_REGISTER_ROUTE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(payload)))
+                .andReturn();
+        MockHttpServletResponse response = result.getResponse();
+        ResponseDTO<User> responseBody = this.mapper.readValue(response.getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {});
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        assertThat(expected.getMessages()).containsAll(responseBody.getMessages());
+    }
+
+    @Order(6)
+    @Test
+    void itShouldReceiveErrorMessageWhenTryingToRegisterNewUserWithBlankEmail() throws Exception {
+        User payload = this.userFactory.setEmail("").newInstance();
+        ResponseDTO<User> expected = new ResponseDTO<>(List.of("É necessário informar um email", "O email deve conter entre 10 e 80 caracteres"), null);
+        MvcResult result = this.mockMvc.perform(post(USER_REGISTER_ROUTE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(payload)))
+                .andReturn();
+        MockHttpServletResponse response = result.getResponse();
+        ResponseDTO<User> responseBody = this.mapper.readValue(response.getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {});
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        assertThat(expected.getMessages()).containsAll(responseBody.getMessages());
+    }
+
+    @Order(7)
+    @Test
+    void itShouldReceiveErrorMessageWhenTryingToRegisterNewUserWithBlankPassword() throws Exception {
+        User payload = this.userFactory.setPassword("").newInstance();
+        ResponseDTO<User> expected = new ResponseDTO<>(List.of("A senha deve conter entre 6 e 70 caracteres", "É necessário informar uma senha"), null);
+        MvcResult result = this.mockMvc.perform(post(USER_REGISTER_ROUTE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(payload)))
+                .andReturn();
+        MockHttpServletResponse response = result.getResponse();
+        ResponseDTO<User> responseBody = this.mapper.readValue(response.getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {});
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        assertThat(expected.getMessages()).containsAll(responseBody.getMessages());
+    }
+
+    @Order(8)
+    @Test
+    void itShouldReceiveErrorMessageWhenTryingToRegisterNewUserWithInvalidEmail() throws Exception {
+        User payload = this.userFactory.setEmail("testtestcom").newInstance();
+        ResponseDTO<User> expected = new ResponseDTO<>(List.of("O email informado é inválido"), null);
+        MvcResult result = this.mockMvc.perform(post(USER_REGISTER_ROUTE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(payload)))
+                .andReturn();
+        MockHttpServletResponse response = result.getResponse();
+        ResponseDTO<User> responseBody = this.mapper.readValue(response.getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {});
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        assertThat(expected.getMessages()).containsAll(responseBody.getMessages());
+    }
+
+    @Order(9)
+    @Test
+    void itShouldReceiveErrorMessageWhenTryingToRegisterNewUserWithAnEmailThatIsAlreadyInUse() throws Exception {
+        User payload = this.userFactory.newInstance();
+        ResponseDTO<User> expected = new ResponseDTO<>(List.of("O email informado está sendo utilizado por outra conta"), null);
+        this.userRepository.save(payload);
+        MvcResult result = this.mockMvc.perform(post(USER_REGISTER_ROUTE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(payload)))
+                .andReturn();
+        MockHttpServletResponse response = result.getResponse();
+        ResponseDTO<User> responseBody = this.mapper.readValue(response.getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {});
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        assertThat(expected.getMessages()).containsAll(responseBody.getMessages());
     }
 }

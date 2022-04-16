c7447202680212b94ff9eaf1976bfde5ed145c48
João <jvitor2000@outlook.com>
2022-04-15 21:20:37 -0300

    feature(#2:backend): Cria um DTO para as credenciais de login.

---
+++ api/src/main/java/api/dtos/LoginDTO.java
@@ -0,0 +1,15 @@
+package api.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class LoginDTO {
+    private String email;
+    private String password;
+}

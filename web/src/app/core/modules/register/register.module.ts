e10c298fabe83a6653eb86f7a2391f1c1f71b743
João <jvitor2000@outlook.com>
2022-04-16 13:48:20 -0300

    feature(#3:frontend): Adiciona header na página de registro

--- web/src/app/core/modules/register/register.module.ts
+++ web/src/app/core/modules/register/register.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from '../../../app.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
 import { RegisterRoutingModule } from './register.routing.module';
+import { HeaderComponent } from '../../../shared/header/header.component';
+import { SharedModule } from '../../../shared/shared.module';
 
 @NgModule({
     declarations: [
         RegisterPageComponent
     ],
     imports: [
-        RegisterRoutingModule
+        RegisterRoutingModule,
+        SharedModule
     ],
     providers: [
     ],

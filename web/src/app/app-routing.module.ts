88351b2b0a804523205f722736cdd4e53734af1d
João <jvitor2000@outlook.com>
2022-04-16 11:30:20 -0300

    feature(#3:frontend): Adiciona o modulo de registro no app-routing

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'register',
+    loadChildren: () => import('./core/modules/register/register.module').then((m) => m.RegisterModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

9aea8e0b19171ec1c1d79e0ccd9a06ea3224e4b0
João <jvitor2000@outlook.com>
2022-04-24 13:09:12 -0300

    feature(#3:frontend): Cria método de autenticação

--- web/src/app/core/modules/login/login-page/login-page.component.ts
+++ web/src/app/core/modules/login/login-page/login-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { credentialsDTO } from 'src/app/core/dtos/credentials';
+import { ToastService } from 'src/app/core/services/toast.service';
+import { AuthService } from 'src/app/core/services/v1/auth.service';
 
 @Component({
   selector: 'app-login-page',
@@ -9,9 +11,30 @@ import { credentialsDTO } from 'src/app/core/dtos/credentials';
 export class LoginPageComponent implements OnInit {
   public form: credentialsDTO = {} as credentialsDTO;
   
-  constructor() { }
+  constructor(private authService: AuthService, private toastService: ToastService) { }
 
   ngOnInit(): void {
   }
 
+  public authenticate(): void {
+    this.authService
+      .authenticate(this.form)
+      .subscribe({
+        next: this.handleSuccessfulAuthentication.bind(this),
+        error: this.handleFailureAuthentication.bind(this)
+      });
+  }
+
+  private handleSuccessfulAuthentication(res: any) {
+    console.log(res);
+  }
+
+  private handleFailureAuthentication(err: any) {
+    if(err.status !== 500) {
+      const messages: Array<string> = err.error.messages;
+      messages.forEach(message => this.toastService.showErrorMessage(message));
+    } else {
+      this.toastService.showErrorMessage('Ocorreu um erro desconhecido. Por favor, tente novamente.');
+    }
+  }
 }

3dd338bf11506a74c733e1aa14858ab6301bec6f
João <jvitor2000@outlook.com>
2022-04-23 22:43:53 -0300

    feature(#3:frontend): Cria método para exibir mensagem de sucesso

--- web/src/app/core/services/toast.service.ts
+++ web/src/app/core/services/toast.service.ts
@@ -5,15 +5,20 @@ import { ToastConfig, Toaster } from 'ngx-toast-notifications';
     providedIn: 'root'
   })
 export class ToastService {
+    private config = {
+        position: 'top-right',
+        preventDuplicates: true
+    } as ToastConfig;
+
     constructor(private toast: Toaster) {}
 
+    public showSuccessMessage(message: string): void {
+        this.config.type = 'success';
+        this.toast.open(message, this.config);
+    }
+
     public showErrorMessage(message: string): void {
-        const config = { 
-            position: 'top-right',
-            type: 'danger',
-            preventDuplicates: true
-         } as ToastConfig;
-         
-        this.toast.open(message, config);
+        this.config.type = 'danger';
+        this.toast.open(message, this.config);
     }
 }
